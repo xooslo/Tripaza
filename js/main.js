df796717e7561110eae1348b350907eade405720
shinyouchan <82295799+shinyouchan@users.noreply.github.com>
2022-06-04 17:08:01 +0900

    image

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let mainText = document.querySelector(".visual-font");
+
+window.addEventListener('scroll' ,function(){
+    let value = window.scrollY;
+    console.log("scrollY" , value);
+    
+    
+
+    if(value > 400) {
+        
+    }
+});
